118ecdcef946ce8736543a596073abf1ade3aa5e
teoshuqi <77791600+teoshuqi@users.noreply.github.com>
2023-07-13 22:17:17 +0800

    change from skimage to opencv2

--- image.py
+++ image.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from skimage import exposure, filters
 from skimage.color import rgb2gray
 from skimage.morphology import square, dilation, erosion
@@ -9,24 +10,27 @@ class Image:
         self.__img = img
 
     def color_to_gray(self):
-        gray = rgb2gray(self.__img)
+        gray = cv2.cvtColor(self.__img, cv2.COLOR_BGR2GRAY)
         gray_img = Image(gray)
         return gray_img
 
-    def denoise(self, sigma=10, truncate=2):
-        denoised = filters.gaussian(self.__img, sigma=sigma, truncate=truncate)
+    def denoise(self, k=10, s=2):
+        denoised = cv2.GaussianBlur(self.__img,
+                                    ksize=(k, k),
+                                    sigmaX=s,
+                                    sigmaY=s)
         denoised_img = Image(denoised)
         return denoised_img
 
-    def clean_white_noise(self, radius=2):
-        kernel = square(radius)
-        eroded = erosion(self.__img, kernel)
-        dilated = dilation(eroded, kernel)
+    def clean_white_noise(self, radius=10):
+        kernel = np.ones((radius, radius), np.uint8)
+        eroded = cv2.erode(self.__img, kernel)
+        dilated = cv2.dilate(eroded, kernel)
         dilated_img = Image(dilated)
         return dilated_img
 
     def format_contrast(self):
-        contrasted = exposure.equalize_hist(self.__img)
+        contrasted = cv2.equalizeHist(self.__img)
         contrasted_img = Image(contrasted)
         return contrasted_img
 
